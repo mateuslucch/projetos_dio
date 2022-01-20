ed3ed7f1c4284e7383c1f8be90bcfefabc8a8a11
mateuslucch <mateuslucch@gmail.com>
2022-01-20 14:21:07 -0300

    Adicionado projeto IFood

---
+++ projeto-ifood/src/Pages/Home/style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+
+
+export const SafeAreaView = styled.SafeAreaView`
+
+        flex:1;
+        align-items: center;
+        padding-top: 40px;
+
+`
+
+export const Image = styled.Image`
+
+    margin: 40px;
+    padding: 80px;
+    
+
+`
+
+export const View = styled.View`
+
+        flex:2;
+        align-items: center;
+        justify-content:center;
+        padding-top: 40px;
+`
+
+export const ButtonContainer = styled.View`
+        flex-direction:row;
+        justify-content: space-around;
+        align-items:center;
+        width:100%;
+        padding: 0 10px 10px 10px;
+`
